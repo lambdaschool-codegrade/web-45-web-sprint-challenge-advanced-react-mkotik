eff2b6d3be3a81364c2b30b9f015229667bbf774
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-08-06 09:07:23 -0400

    rendered plants to screen

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { isCompositeComponent } from "react-dom/test-utils";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-      .then(res => {
-        
-      });
+    axios
+      .get("http://localhost:3333/plants")
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          plants: res.data,
+        });
+        console.log(this.state.plants);
+      })
+      .catch((err) => console.log(err));
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
